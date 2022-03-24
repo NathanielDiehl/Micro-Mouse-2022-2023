6dc81d127d202945b3d4835c939f15b3b5ba604a
Nathan Diehl <33075406+NathanielDiehl@users.noreply.github.com>
2022-03-23 20:27:26 -0500

    added hardware classes

--- Project Micromouse/World.cpp
+++ Project Micromouse/World.cpp
@@ -1,20 +1,24 @@
 #include <vector>
 
 class World {
+    private: 
+        static float MazeWalls[16][16];
+
     public:
         static std::vector <float> RobotPosition (2);
         static float RobotRotation;
 
-        static float MazeWalls[16][16];
 
 
-
-
-        static void init() {
+        static void Init() {
             RobotPosition.at(0) = 0;
             RobotPosition.at(1) = 0;
             RobotRotation = 0;
 
+            ClearWalls();
+        }
+
+        static void ClearWalls(){
             for (int i = 0; i < 16; i++) {
                 for (int j = 0; j < 16; j++) {
                     MazeWalls[i][j] = 0;
@@ -22,8 +26,14 @@ class World {
             }
         }
 
+        static bool IsWallAt(int row, int col) {
+            if(row >= 0 && col >= 0 && row < 16 && col < 16)
+                return MazeWalls[row][col] == 1;
+        }
+
         static void FoundWall(int row, int col) {
-            MazeWalls[row][col] = 1;
+            if(row >= 0 && col >= 0 && row < 16 && col < 16)
+                MazeWalls[row][col] = 1;
         }
 
 
